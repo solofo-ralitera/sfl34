f6fbfb9bb20a6c11b44eb8b8569ea6d4c78d73ba
Solofo <solofo.ralitera@gmail.com>
2018-05-10 19:19:29 +0300

    Entity example

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,42 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Todo;
+use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+	/**
+	 * @Route("/addtodo", name="addtodo")
+	 *
+	 * @param EntityManager $em
+	 * @return JsonResponse
+	 */
+	public function todoAction(EntityManager $em)
+	{
+		$todo = new Todo();
+		$em->persist($todo);
+		$em->flush();
+
+		return new JsonResponse([
+			'id' => $todo->getId(),
+		]);
+	}
 }
---
+++ src/AppBundle/Entity/Todo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="todo")
+ */
+class Todo {
+	/**
+	 * @ORM\Column(type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	private $id;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
